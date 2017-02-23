55ce0b5d26bf39af66f0a96cb022e4f0e2c8f562
Shomrina Marina <milagrous@gmail.com>
2017-02-23 21:36:17 +0300

    Сделан тест на логин в админ панель
    тестовый метод MyFirstTest в классе AppTest изменен. Раньше там
    открывалась страница поиска гугл, теперь он логинится в административную
    панель магазина http://localhost/litecart/admin

--- seleniumWebDriver/src/test/java/MyFirstTest/AppTest.java
+++ seleniumWebDriver/src/test/java/MyFirstTest/AppTest.java
@@ -4,10 +4,12 @@ package MyFirstTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -21,13 +23,16 @@ public class AppTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest() {
-        driver.get("http://www.google.com/");
-
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
