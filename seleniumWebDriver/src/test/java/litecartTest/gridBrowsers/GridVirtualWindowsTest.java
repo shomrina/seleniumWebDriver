79bdf0a45cd0c57aed4e6c7ada6886cc9bffc773
Shomrina Marina <milagrous@gmail.com>
2017-03-19 20:35:38 +0300

    Сделан простой грид
    [+] Задание 15. Постройте небольшой грид
    Установите виртуальную машину, внутри которой работает Windows,
    и создайте грид, который состоит из диспетчера, работающего на вашей
    основной машине,
    и двух узлов -- один тоже на основной машине, а другой внутри
    виртуальной машины.
    
    Настройте узлы так, чтобы в виртуальной машине был доступен браузер
    Internet Explorer,
    а на основной машине, наоборот, он был недоступен.
    
    Попробуйте запустить какие-нибудь тесты удалённо на этом гриде, указывая
    разные браузеры,
    и убедитесь, что Internet Explorer действительно запускается внутри
    виртуальной машины,
    а другие браузеры, наоборот, на вашей основной машине.
    
    Можно использовать любую систему виртуализации,
    но если у вас нет предпочтений -- берите https://www.virtualbox.org/
    
    Готовые образы Windows для разных систем виртуализации можно найти
    здесь:
    https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/

--- seleniumWebDriver/src/test/java/litecartTest/gridBrowsers/GridVirtualWindowsTest.java
+++ seleniumWebDriver/src/test/java/litecartTest/gridBrowsers/GridVirtualWindowsTest.java
@@ -1,8 +1,11 @@
 package litecartTest.gridBrowsers;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,6 +13,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Marina on 19.03.2017.
@@ -20,26 +24,46 @@ public class GridVirtualWindowsTest {
     private RemoteWebDriver driverVirtual;
     private WebDriverWait waitVirtual;
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true)
     public void setupBrowsersDriver() throws MalformedURLException {
         DesiredCapabilities desiredCapabilitiesVirtual;
         desiredCapabilitiesVirtual = DesiredCapabilities.internetExplorer();
-        desiredCapabilitiesVirtual.setCapability("platform", "VISTA");
+
+       /* desiredCapabilitiesVirtual.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+      //  desiredCapabilitiesVirtual.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "http://www.google.com");
+        desiredCapabilitiesVirtual.setCapability("ignoreProtectedModeSettings", true);
+        desiredCapabilitiesVirtual.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        desiredCapabilitiesVirtual.setJavascriptEnabled(true);
+        desiredCapabilitiesVirtual.setCapability("requireWindowFocus", true);
+        desiredCapabilitiesVirtual.setCapability("enablePersistentHover", false);
+        desiredCapabilitiesVirtual.setCapability("requireWindowFocus", true);*/
+
+        desiredCapabilitiesVirtual.setCapability("platform", "VISTA");  //VISTA
         driverVirtual = new RemoteWebDriver(new URL("http://192.168.1.39:4444/wd/hub"), desiredCapabilitiesVirtual);
-        waitVirtual = new WebDriverWait(driverVirtual, 10);
+//        driverVirtual.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+//        driverVirtual.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        waitVirtual = new WebDriverWait(driverVirtual, 10);
     }
 
     @Test
-    public void testGridIeOnVirtual() {
-        driverVirtual.get("http://localhost/litecart/admin/");
+    public void testGridIeOnVirtual() throws InterruptedException {
+        driverVirtual.get("http://192.168.1.39/litecart/admin/");
+   //     Thread.sleep(5000L);
+   //     waitElementPresence(By.name("username"), 30);
         driverVirtual.findElement(By.name("username")).sendKeys("admin");
         driverVirtual.findElement(By.name("password")).sendKeys("admin");
         driverVirtual.findElement(By.name("login")).click();
+        Thread.sleep(3000L);
     }
 
-    @AfterClass
+    @AfterClass(alwaysRun = true)
     public void stop() {
         driverVirtual.quit();
         driverVirtual = null;
+    }
+
+    public void waitElementPresence(By element, int timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driverVirtual, timeOutInSeconds);
+        wait.until(ExpectedConditions.presenceOfElementLocated(element));
     }
 }
