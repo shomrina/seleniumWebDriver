aa64fc70e88560de3aa155b92a494e72441a0158
Shomrina Marina <milagrous@gmail.com>
2017-03-23 01:49:06 +0300

    Сделан кейс прощелкивания страниц товаров в админке
    подготовка к 17 заданию.
    Тепреь необходимо реализовать часть для протоколирования и добавить ее
    сюда

--- seleniumWebDriver/src/test/java/litecartTest/appTests/adminTest/EventFiringCatalogTest.java
+++ seleniumWebDriver/src/test/java/litecartTest/appTests/adminTest/EventFiringCatalogTest.java
@@ -3,13 +3,16 @@ package litecartTest.appTests.adminTest;
 import litecartTest.appTests.framework.LoginAdminPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -35,6 +38,37 @@ public class EventFiringCatalogTest {
             //go to page Catalog
         driver.findElement(By.xpath("//*[text() = 'Catalog']")).click();
         waitAllElementVisibility(By.xpath(".//*[@class='dataTable']"), 20);
+            //
+        driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Rubber Ducks')]")).click();                       //click by Rubber Ducks
+        waitElementVisibility(driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Subcategory')]")), 5);  //wait other folder
+        driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Subcategory')]")).click();                    //click by Subcategory
+        waitAllElementVisibility(By.xpath(".//*[@class='dataTable']//td//a[contains(@href,'product_id')]"), 20);   //wait
+
+        List<WebElement> goodsList = driver.findElements(By.xpath(".//*[@class='dataTable']//td[3]//a[contains(@href,'product_id')]"));  //выбрать в таблице все объекты, которые продукты
+        System.out.println("goodList size = " + goodsList.size());
+        for (int i = 0; i < goodsList.size(); i++) {
+            if (i != 0) {
+                driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Rubber Ducks')]")).click();                       //click by Rubber Ducks
+                waitElementVisibility(driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Subcategory')]")), 5);  //wait other folder
+                driver.findElement(By.xpath(".//*[@class='dataTable']//td//a[contains(text(),'Subcategory')]")).click();                    //click by Subcategory
+                waitAllElementVisibility(By.xpath(".//*[@class='dataTable']//td//a[contains(@href,'product_id')]"), 20);   //wait
+            }
+
+            WebElement good = driver.findElements(By.xpath(".//*[@class='dataTable']//td[3]//a[contains(@href,'product_id')]")).get(i);
+
+            System.out.println("i = [" + i + "]: href = "
+                    + good.getAttribute("href")
+            + ", good name = " + good.getText());
+
+            good.click();  //открыть страницу товара
+            waitAllElementVisibility(By.xpath(".//*[@name='name[en]']"), 20);                               //ожидание
+
+            //todo здесь вставить проверку не появляются ли в логе браузера сообщения (любого уровня)
+
+            driver.findElement(By.xpath(".//*[@name='cancel']")).click();
+            waitAllElementVisibility(By.xpath(".//*[@class='dataTable']"), 20);
+        }
+
 
 
 
@@ -51,6 +85,15 @@ public class EventFiringCatalogTest {
     public void waitAllElementVisibility(By element, int timeOutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element));
+    }
+
+    public void waitElementVisibility(WebElement element, int timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+
     }
 
 }
